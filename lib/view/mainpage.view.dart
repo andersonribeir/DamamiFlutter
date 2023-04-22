0a2478e427d5024e0ae933b2881532aa1e4d54c2
andersonluiss <anderson.luis@techwayinfo.com.br>
2023-04-22 00:53:52 -0300

    ok

---
+++ lib/view/mainpage.view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+  @override
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(
+      backgroundColor: Colors.pink,
+    );
+  }
+}
